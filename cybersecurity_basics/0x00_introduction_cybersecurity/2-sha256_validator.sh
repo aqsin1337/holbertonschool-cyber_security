548c019a3aaa62339ff25f8e17cbd580c2b2123b
aqsin1337 <aqsin19999999@gmail.com>
2026-05-09 12:54:44 -0400

    fix task 2 and 4 again

--- cybersecurity_basics/0x00_introduction_cybersecurity/2-sha256_validator.sh
+++ cybersecurity_basics/0x00_introduction_cybersecurity/2-sha256_validator.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-echo "$1: $(sha256sum $1 | grep -c $2 | sed 's/1/OK/;s/0/FAIL/')"
+echo "$2  $1" | sha256sum -c 2>/dev/null | sed 's/FAILED/FAIL/'
--- cybersecurity_basics/0x00_introduction_cybersecurity/4-root_process.sh
+++ cybersecurity_basics/0x00_introduction_cybersecurity/4-root_process.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-ps -u $1 -o user,pid,%cpu,%mem,vsz,rss,tty,stat,start,time,cmd | grep -vE " +0 +0 "
+ps -u $1 -o user,pid,%cpu,%mem,vsz,rss,tty,stat,start,time,cmd | grep -vE "^\S+ +\S+ +\S+ +\S+ +0 +0 "
