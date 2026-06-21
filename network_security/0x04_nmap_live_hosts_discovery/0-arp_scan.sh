5d02de1b83ce3fa654f58f4e7f0dcbfb37f7d5e9
aqsin1337 <aqsin19999999@gmail.com>
2026-06-21 17:33:11 +0000

    Use sudo inside script for arp scan

--- network_security/0x04_nmap_live_hosts_discovery/0-arp_scan.sh
+++ network_security/0x04_nmap_live_hosts_discovery/0-arp_scan.sh
@@ -1,16 +1,2 @@
-#!/usr/bin/env bash
-#
-# Scans a subnetwork to discover live hosts using an ARP scan (nmap).
-# Usage: sudo ./0-arp_scan.sh <subnetwork>
-
-if [[ "$EUID" -ne 0 ]]; then
-    echo "Error: this script must be run as root (or with sudo)." >&2
-    exit 1
-fi
-
-if [[ -z "$1" ]]; then
-    echo "Usage: $0 <subnetwork>" >&2
-    exit 1
-fi
-
-nmap -PR -sn -n "$1"
+#!/bin/bash
+sudo nmap -sn -PR $1
