ab68fa0aa14b91eceed1c1e94e7e03efe587cc38
aqsin1337 <aqsin19999999@gmail.com>
2026-07-05 08:16:31 +0000

    Fix xor decoder to pass all test cases securely

--- web_application_security/0x01_owasp_top_10/1-xor_decoder.sh
+++ web_application_security/0x01_owasp_top_10/1-xor_decoder.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
-# Decode WebSphere XOR encoded hashes safely
+# Decode WebSphere XOR encoded hashes according to Betty style
 
 INPUT="$1"
 CLEAN_HASH=$(echo "$INPUT" | sed 's/^{xor}//')
 BASE64_DECODED=$(echo "$CLEAN_HASH" | base64 -d 2>/dev/null)
 
+len=${#BASE64_DECODED}
 index=0
-while [ $index -lt ${#BASE64_DECODED} ]
+
+while [ "$index" -lt "$len" ]
 do
-    char="${BASE64_DECODED:$index:1}"
+    char=$(expr substr "$BASE64_DECODED" $((index + 1)) 1)
     if [ -z "$char" ]
     then
         ascii=0
