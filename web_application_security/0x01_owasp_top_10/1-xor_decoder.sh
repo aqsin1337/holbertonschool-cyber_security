25df66b29f745f20b4370ca564ab60f74af49db3
aqsin1337 <aqsin19999999@gmail.com>
2026-07-05 08:15:42 +0000

    Fix null byte handling in xor decoder

--- web_application_security/0x01_owasp_top_10/1-xor_decoder.sh
+++ web_application_security/0x01_owasp_top_10/1-xor_decoder.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Decode WebSphere XOR encoded hashes
+# Decode WebSphere XOR encoded hashes safely
 
 INPUT="$1"
 CLEAN_HASH=$(echo "$INPUT" | sed 's/^{xor}//')
@@ -9,7 +9,12 @@ index=0
 while [ $index -lt ${#BASE64_DECODED} ]
 do
     char="${BASE64_DECODED:$index:1}"
-    printf -v ascii '%d' "'$char"
+    if [ -z "$char" ]
+    then
+        ascii=0
+    else
+        printf -v ascii '%d' "'$char"
+    fi
     xor_result=$((ascii ^ 95))
     printf "\\$(printf '%03o' "$xor_result")"
     index=$((index + 1))
